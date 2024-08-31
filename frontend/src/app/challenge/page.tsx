a50dccc3b1510a7d6e72f4ae8e1903bd61d334c8
MattWong-ca <matthew.yt.wong@gmail.com>
2024-08-30 22:22:30 -0230

    start challenge bet UI

---
+++ frontend/src/app/challenge/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from 'react';
+
+export default function ChallengePage() {
+  return (
+    <div>
+      <h1>Challenge Page</h1>
+      {/* Add your challenge page content here */}
+    </div>
+  );
+}
