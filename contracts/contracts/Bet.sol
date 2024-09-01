2e1e03b3c536aa18659144dd3faaea1b5340e3a9
MattWong-ca <matthew.yt.wong@gmail.com>
2024-08-31 23:52:09 -0400

    re-add withdraw fxn

--- contracts/contracts/Bet.sol
+++ contracts/contracts/Bet.sol
@@ -34,6 +34,27 @@ contract Bet {
         betAddresses.push(msg.sender);
     }
 
+    // Winner withdraws their winnings
+    // Instead of finding bet using _player1, can use db to find cast hash + bet amount
+    function withdraw(address _player1) external {
+        BetInfo storage betInfo = bets[_player1];
+
+        require(betInfo.isChallenged, "Bet has not been challenged");
+        require(
+            msg.sender == _player1 || msg.sender == betInfo.player2,
+            "Only players involved in the bet can withdraw"
+        );
+
+        uint256 totalAmount = betInfo.amount * 2;
+        require(
+            address(this).balance >= totalAmount,
+            "Not enough funds in contract"
+        );
+
+        delete bets[_player1]; // Reset the bet info after payout
+        payable(msg.sender).transfer(totalAmount);
+    }
+
     // Function to get active bets for the connected wallet
     function getActiveBets(address user) external view returns (BetInfo[] memory) {
         uint256 count = 0;
@@ -59,5 +80,10 @@ contract Bet {
         }
 
         return activeBets;
+    }
+
+    // Get contract balance
+    function getBalance() external view returns (uint256) {
+        return address(this).balance;
     }
 }
